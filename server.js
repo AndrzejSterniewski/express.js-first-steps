577c2d5868dec4aeb0b2287ce459ef16a70a3483
Andrzej Sterniewski <drapiszon@gmail.com>
2024-01-27 19:30:59 +0100

    delete comments

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const hbs = require('express-handlebars');
 
 const app = express();
 
-// app.engine('hbs', hbs());
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', 'hbs');
 
@@ -41,29 +40,3 @@ app.use((req, res) => {
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
 });
-
-
-
-
-// use example
-
-// app.use('/admin', (req, res, next) => {
-//     if(isAdmin()) next();
-//     else res.send('Go away!');
-// });
-
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, '/views/home.html'));
-// });
-
-// app.get('/cart', (req, res) => {
-//     res.sendFile(path.join(__dirname, '/views/cart.html'));
-// });
-
-// app.get('/admin/products', (req, res) => {
-//     res.sendFile(path.join(__dirname, '/views/admin/products.html'));
-// });
-
-// app.get('/admin/payments', (req, res) => {
-//     res.sendFile(path.join(__dirname, '/views/admin/payments.html'));
-// });
