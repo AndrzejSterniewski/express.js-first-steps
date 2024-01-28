b30506ee1f3dd270a8f530f089124411b48661cf
Andrzej Sterniewski <drapiszon@gmail.com>
2024-01-28 13:45:07 +0100

    change dark.hbs layout

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
 
 const app = express();
 
@@ -8,35 +10,50 @@ app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.post('/contact/send-message', upload.single('image'), (req, res) => {
+
+    const { author, sender, title, message } = req.body;
+    const image = req.image;
+
+    if(author && sender && title && message && image) {
+        res.render('contact', { isSent: true });
+      }
+      else {
+        res.render('contact', { isError: true });
+      }
+});
 
 app.get('/', (req, res) => {
-  res.render('index');
+    res.render('index');
 });
 
 app.get('/hello/:name', (req, res) => {
-  res.render('hello', { name: req.params.name });
+    res.render('hello', { name: req.params.name });
 });
 
 app.get('/about', (req, res) => {
-  res.render('about');
+    res.render('about');
 });
 
 app.get('/contact', (req, res) => {
-  res.render('contact');
+    res.render('contact');
 });
 
 app.get('/info', (req, res) => {
-  res.render('info');
+    res.render('info');
 });
 
 app.get('/history', (req, res) => {
-  res.render('history', { layout: 'dark' });
+    res.render('history', { layout: 'dark' });
 });
 
 app.use((req, res) => {
-  res.status(404).send('404 not found...');
+    res.status(404).send('404 not found...');
 })
 
 app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
+    console.log('Server is running on port: 8000');
 });
